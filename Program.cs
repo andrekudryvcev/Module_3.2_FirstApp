a73dabe7564d87b58c514dd7ea65279d5ad911fc
Андрей Кудрявцев <andrekudryvcev@yahoo.com>
2022-04-01 20:56:22 +0500

    the task  3.3.2

--- Program.cs
+++ Program.cs
@@ -4,24 +4,17 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        const string myName = "Nemo";
+        string MyName = "Nemo";
+        byte MyAge = 35;
+        bool HaveIAPet = true;
+        double MyShoeSize = 37.5;
 
-        Console.WriteLine(myName);
+        Console.WriteLine("My name is {0}", MyName);
+        Console.WriteLine("My age is {0}", MyAge);
+        Console.WriteLine("Do I have a pet? {0}", HaveIAPet);
+        Console.WriteLine("My shoe size is {0}", MyShoeSize);
 
-        Console.WriteLine("\tПривет мир");
-        Console.WriteLine("\tМне 35 лет");
-        Console.WriteLine("\tMy name is\n {0}", myName);
-        Console.WriteLine("\u0040");
-        Console.WriteLine("\x23");
-
-        Console.WriteLine(true);
-        Console.WriteLine(false);
-
-        Console.WriteLine(5);
-
-        Console.WriteLine(0x0A);
-        Console.WriteLine(0b11);
-
+        
         Console.ReadKey();
     }
 }
