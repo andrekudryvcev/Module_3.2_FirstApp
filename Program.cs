f064b3b9c4d24ff74be766163b1af3fff1156781
Андрей Кудрявцев <andrekudryvcev@yahoo.com>
2022-04-01 14:31:07 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string myName;
+        myName = "Nemo";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
