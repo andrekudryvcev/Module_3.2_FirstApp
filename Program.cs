13560786d20c0fb2de2e264f4c2410904642193e
Андрей Кудрявцев <andrekudryvcev@yahoo.com>
2022-04-01 20:32:03 +0500

    Финальный коммит юнита 3.2

--- Program.cs
+++ Program.cs
@@ -4,10 +4,23 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        string myName;
-        myName = "Nemo";
+        const string myName = "Nemo";
 
         Console.WriteLine(myName);
+
+        Console.WriteLine("\tПривет мир");
+        Console.WriteLine("\tМне 35 лет");
+        Console.WriteLine("\tMy name is\n {0}", myName);
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\x23");
+
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+
+        Console.WriteLine(5);
+
+        Console.WriteLine(0x0A);
+        Console.WriteLine(0b11);
 
         Console.ReadKey();
     }
